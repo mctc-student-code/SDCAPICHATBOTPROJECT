51cc202c65c18ab1d33b2cee4dcca77994dc1d7b
Javier Reyes Herrera <J4vierR3yes11@gmail.com>
2025-11-21 05:07:07 -0600

    added flask frame

--- flaskapp.py
+++ flaskapp.py
@@ -1,2 +1,15 @@
 from flask import Flask
 from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/scheduler')
+def scheduler():
+
+
+if __name__ == "__main__":
+    app.run()
