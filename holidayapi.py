6046db0718f7c3e7ba454f7c5f55cedb370feafe
Zcaroll <zcarroll.bus@gmail.com>
2025-11-25 09:54:34 -0600

    validation
    Added input validation for year, month, and day.

--- holidayapi.py
+++ holidayapi.py
@@ -1,18 +1,57 @@
 import requests
 import pprint
+import os
 
 # https://holidays.abstractapi.com/v1/?api_key=&country=US&year=2025&month=12&day=22 example structure with API key missing
 api_key = "" #apit key TODO move to env variable
 
 # User inputs
 country = input("Enter country code (e.g., US): ")
-year = "2025" # static for now
-month = input("Enter month (1-12): ")
-day =   input("Enter day (1-31): ")
+# year = "2025" # static for now
+# month = input("Enter month (1-12): ")
+# day =   input("Enter day (1-31): ")
 
-#TODO: validate user inputs
+# validate user inputs
 
-# Holiday API url without the infor
+#year
+while True:
+    try:
+        year = int(input("Enter year : "))
+        if 0 < year < 3000:  # Month must be between 1 and 12
+                break
+        else:
+            print("Year must be positive.")
+
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+#month
+while True:
+    try:
+        month = int(input("Enter month (1-12): "))
+        if 0 < month < 13:  # Month must be between 1 and 12
+                break
+        else:
+            print("Month must be between 1 and 12.")
+
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+#day
+while True:
+    try:
+        day =   int(input("Enter day (1-31): "))
+        if 0 < day < 32:  # Day must be between 1 and 31
+                break
+        else:
+            print("Day must be between 1 and 31.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+
+
+
+# Holiday API url without the info
 base_url = "https://holidays.abstractapi.com/v1/?"
 
 # completed URL with user inputs
